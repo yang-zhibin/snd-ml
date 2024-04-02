04cbe06aae81c211b1f0f3ce5181bc2c4e26191a
Zhibin Yang <zhibin.yang@cern.ch>
2024-04-02 12:02:35 +0200

    completed reading hits2MCPoints

--- data_examination/check_hits2MCPoints.py
+++ data_examination/check_hits2MCPoints.py
@@ -7,22 +7,39 @@ def check_with_TFile():
     f = ROOT.TFile(file_path, 'read')
     tree = f.Get('cbmsim')
 
+
     for event in tree:
         hit2MC =event.Digi_ScifiHits2MCPoints
         scifi_hits =event.Digi_ScifiHits
 
 
-        #print(dir(hit2MC))
+        for aHit in scifi_hits:
+
+            detID = aHit.GetDetectorID()
+            print("detId", detID)
+            print("hit to mc")
+            print(hit2MC[0].wList(detID))
+            break
         print("length of h2mc",len(hit2MC))
         print("length of hits", len(scifi_hits))
 
+        print("methods in Digi_ScifiHits2MCPoints: ")
+        print(dir(hit2MC[0]))
         print(dir(hit2MC))
 
-            
-            
+        print("Dumping of Digi_ScifiHits2MCPoints")
+        hit2MC.Dump()
+
+        print(type(hit2MC))
+        print(type(hit2MC[0]))
         
         break
+
+        
+        
 
 if __name__ == "__main__":
     print("checking hits2MCPoints")
     check_with_TFile()
+
+    
