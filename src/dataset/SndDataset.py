7170ac4955388240795102499640cf0c63b5bb54
Zhibin Yang <zhibin.yang@cern.ch>
2024-05-30 11:54:06 +0200

    working on padding

--- src/dataset/SndDataset.py
+++ src/dataset/SndDataset.py
@@ -5,12 +5,12 @@ import uproot
 import pandas as pd
 import numpy as np
 
-def pad_point_cloud(point_cloud, max_points=4096):
+def pad_point_cloud(point_cloud, feature_len, max_points=4096):
     """Pad the point cloud to max_points with zero-vectors."""
     #print(point_cloud.dtype)
     padding = max_points - point_cloud.shape[1]
     if padding > 0:
-        pad_tensor = torch.zeros((7, padding), dtype=point_cloud.dtype)
+        pad_tensor = torch.zeros((feature_len, padding), dtype=point_cloud.dtype)
         point_cloud = torch.cat([point_cloud, pad_tensor], dim=1)
     else:
         point_cloud = point_cloud[:,:max_points]
@@ -61,46 +61,37 @@ class SndDataset(Dataset):
                 y = 1
             else:
                 y = 0
+        y=torch.tensor([y])
+
+        # id ('runId', 'eventId')
+        ids = [event_data['runId'][0],event_data['eventId'][0]]
 
         # features
         
-        hits_features_name = ['Hits.orientation','Hits.x1', 'Hits.y1', 'Hits.z1', 'Hits.x2', 'Hits.y2', 'Hits.z2']
+        hits_features_name = ['Hits.detType','Hits.orientation','Hits.x1', 'Hits.y1', 'Hits.z1', 'Hits.x2', 'Hits.y2', 'Hits.z2']
         event_features_name = ['RecoMuon.px', 'RecoMuon.py', 'RecoMuon.pz', 'RecoMuon.x', 'RecoMuon.y', 'RecoMuon.z']
         features = []
         for feature in hits_features_name:
             features.append(np.squeeze(event_data[feature].to_numpy()))
+        
+        pad_features = pad_point_cloud(torch.Tensor(np.array(features)), len(hits_features_name))
+        
 
         use_event_feature = False
         if (use_event_feature):
-            for feature in event_features_name:
-                features.append(np.squeeze(event_data[feature].to_numpy))
+            # Create a combined list of all feature names
+            all_features_name = hits_features_name + event_features_name
 
-        # id ('runId', 'eventId')
-        ids = [event_data['runId'][0],event_data['eventId'][0]]
-        
-        #print(id)
-        #print(y)
-        #print(np.asarray(features,dtype="object").shape)
-        #print(features)
-        # print(np.asarray(event_features).shape)
-        # print(event_features)
+            # Create a list of tensors from the event_data dictionary using a list comprehension for all features
+            tensors = [torch.tensor(event_data[feature], dtype=torch.float32) for feature in all_features_name]
+        else:
+            tensors = [torch.tensor(event_data[feature], dtype=torch.float32) for feature in hits_features_name]
 
+        x = torch.squeeze(torch.stack(tensors)).T
 
-        # features  = np.stack(hit_features, event_features)
-        # print(np.asarray(features).shape)
-        # print(features)
-        #print(features)
+        #print("stack x", x.shape)
+        #print("pad x",pad_features.T.shape)
 
-        pad_features = pad_point_cloud(torch.Tensor(np.array(features)))
-        #pad_x = torch.stack(pad_features)
-        #print(pad_x.shape)
-
-
-        
-        x = pad_features.T
-        #print("x:",x.shape)
-        y=torch.tensor([y])
-        #print("y",y.shape)
 
         return Data(x=x, y=y, ids=ids)
 
