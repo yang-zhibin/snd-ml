5aa98a7b095cde30a9a03de14cd85a131607a7a8
Zhibin Yang <zhibin.yang@cern.ch>
2024-06-25 16:15:15 +0200

    add fiducial cut information

--- data/convertedData/EventClasses.h
+++ data/convertedData/EventClasses.h
@@ -5,8 +5,9 @@
 
 class Id : public TObject {
 public:
-    Int_t runId, eventId;
-    Id(Int_t runId=-1, Int_t eventId=-1) : runId(runId), eventId(eventId) {}
+    Int_t runId, eventId, partitionId;
+    Id(Int_t runId=-1, Int_t eventId=-1, Int_t partitionId=-1) : 
+    runId(runId), eventId(eventId), partitionId(partitionId) {}
     virtual ~Id() {}
 
     ClassDef(Id, 1)  
@@ -18,6 +19,9 @@ public:
     int pdgCode;    // PDG code identifying the particle type
     float px, py, pz; // momentum components
     float x, y, z;    // coordinates of the first interaction point
+    double scifi_avg_ver, scifi_avg_hor;
+    double DS_avg_ver, DS_avg_hor;
+
 
     Label(int pdgCode = 0,
           float px = 0.0, float py = 0.0, float pz = 0.0,
@@ -66,24 +70,24 @@ public:
     float x2, y2, z2; // coordinates of the other end
     int detType;      // detector type 1: scifi, 2: us, 3: ds
     float hitTime;    // time of the hit
-    std::vector<MCTrack> tracks;
+    int detId;         // detector Id
+
 
     Hit(bool orientation = true, 
         float x1 = 0.0, float y1 = 0.0, float z1 = 0.0,
         float x2 = 0.0, float y2 = 0.0, float z2 = 0.0,
         int detType = 0, 
-        float hitTime = 0.0) 
+        float hitTime = 0.0, 
+        int detId = 0) 
     : orientation(orientation), 
       x1(x1), y1(y1), z1(z1), 
       x2(x2), y2(y2), z2(z2), 
       detType(detType), 
-      hitTime(hitTime){}
+      hitTime(hitTime),
+      detId(detId){}
 
     virtual ~Hit() {}
     
-    void addTrack(const MCTrack& track) {
-        tracks.push_back(track);
-    }
     ClassDef(Hit, 1)
 };
 
@@ -93,6 +97,7 @@ public:
     float x1, y1, z1; // coordinates of one end
     float x2, y2, z2; // coordinates of the other end
     int detType;      // detector type
+
 
     // Constructor with default parameter values
     ScifiCluster(bool orientation = true,
