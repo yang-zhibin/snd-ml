ca82237f2383d696296906beb4c22452c26ed07c
Zhibin Yang <zhibin.yang@cern.ch>
2025-01-20 14:17:42 +0100

    snakemake first attempt

---
+++ snakemake/HTCondor_profile/condor-jobscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# properties = {properties}
+# 
+# condor-jobscript.sh
+#
+# Wrapper for snakemake jobs
+#
+# Copyright (C) 2019 Matthew Stone <mrstone3@wisc.edu>
+# Distributed under terms of the MIT license.
+
+{exec_job}
